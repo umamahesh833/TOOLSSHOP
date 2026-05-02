776487eb7fc54791fbd9fbbe2307fb668081cb83
uma mahesh Kadiyala <umamahesh833@gmail.com>
2026-05-02 10:13:23 +0100

    First push of the project

---
+++ tests/FirstUItest.spec.js
@@ -0,0 +1,77 @@
+import{test, expect} from '@playwright/test'
+
+
+test('My first mahesh Test ',async ({page})=>{
+//Launch the browser and navigate to our application
+await page.goto("https://practicesoftwaretesting.com/")
+await page.waitForTimeout(5000)
+await expect(page).toHaveTitle(/Testing/);
+
+await page.getByText("Sign in").click()
+
+
+// await page.getByPlaceholder('Your email').fill("Mahesh")
+// await page.getByPlaceholder('Your password').fill("test123")
+// await page.waitForTimeout(2000)
+
+// await page.getByRole("button", {name:"Login"}).click()
+await page.waitForTimeout(2000)
+
+await page.getByRole("link", {name:"Register your account"}).click()
+
+//Entering firname
+await page.locator("input#first_name").fill("Automation")
+await page.locator("input[data-test='last-name']").fill("Automation")
+await page.waitForTimeout(2000)
+
+
+//Selecting an item from a drop down
+const CoutryDropDown = page.locator('#country')
+await CoutryDropDown.selectOption("Denmark")//selecting item using label
+await page.waitForTimeout(2000)
+// await CoutryDropDown.selectOption("FR")//selecting item using value
+// await page.waitForTimeout(2000)
+// await CoutryDropDown.selectOption({index:5})//selecting item using index
+
+
+
+await page.locator("//button[@type='submit']").click()
+
+const DoBerror = page.locator("//div[@id='dob-error']/div[2]")
+console.log("error message - "+await DoBerror.textContent())
+})
+
+
+
+test('Radio Button Test ',async ({page})=>{
+//Launch the browser and navigate to our application
+await page.goto("https://automationexercise.com/signup")
+await page.waitForTimeout(5000)
+await page.getByRole("button", {name:"Consent"}).click()
+await page.getByPlaceholder("Name").fill("Random9987@gmail.com")
+await page.waitForTimeout(1000)
+await page.locator("[data-qa='signup-email']").fill("Random9987@gmail.com")
+
+const SignUp = page.getByRole("button", {name:'Signup'})
+await SignUp.click()
+
+//select a radio button
+
+const mrRadio = page.locator('#id_gender1')
+const mrsRadio = page.locator('#id_gender2')
+await mrRadio.click()
+
+
+console.log("the status of radio button Mr is - "+await mrRadio.isChecked())
+console.log("the status of radio button Mrs is - "+await mrsRadio.isChecked())
+
+
+//Select a check box
+const NewsLetterCheckBox = page.locator('#newsletter')
+await NewsLetterCheckBox.click()
+await page.waitForTimeout(3000)
+await NewsLetterCheckBox.uncheck()
+})
+
+
+
---
+++ tests/carttests.spec.js
@@ -0,0 +1,83 @@
+import { test, expect } from '@playwright/test';
+
+
+
+
+
+
+test.only('Validate product add to cart',async ({page})=>{
+
+//Launch the application
+await page.goto('https://practicesoftwaretesting.com/')
+
+
+const Hammer = page.locator("[alt='Hammer']")
+const addtoCart = page.getByRole("button", {name:'Add to cart'})
+const alert = page.locator("[role='alert']")
+const CartIcon = page.locator("[data-test='nav-cart']")
+const productTitle = page.locator(".product-title")
+const productPrice = page.locator("[data-test='product-price']")
+
+//Click on a product
+await Hammer.click()
+
+//Click on add to cart button
+await addtoCart.click()
+
+//Validate alert is displaying
+await expect(alert).toBeVisible();
+
+//Click on cart icon
+await CartIcon.click()
+
+//Capture the title of the product
+ const productname = await productTitle.textContent()
+// console.log(productname)
+await page.pause()
+expect(productname).toBe('Hammer');
+
+
+
+await expect(productTitle).toHaveText('Hammer');
+await expect(productPrice).toHaveText('$12.58');
+
+})
+
+
+
+test.only('Add multiple products to cart and validate in cart page',async ({page})=>{
+
+//Launch the application
+await page.goto('https://practicesoftwaretesting.com/')
+
+const Products = page.locator(".card-img-top")
+const addtoCart = page.getByRole("button", {name:'Add to cart'})
+const CartIcon = page.locator("[data-test='nav-cart']")
+const alert = page.locator("[role='alert']")
+const HomePge = page.getByRole("link",{name:"Home"})
+const NoofProduct = page.locator("//table/tbody/tr")
+
+await Products.first().click()
+await addtoCart.click()
+//Validate alert is displaying
+await expect(alert).toBeVisible();
+await HomePge.click()
+
+await page.waitForTimeout(2000)
+
+await Products.nth(4).click()
+await addtoCart.click()
+//Validate alert is displaying
+await expect(alert).toBeVisible();
+await HomePge.click()
+await page.waitForTimeout(2000)
+
+await Products.last().click()
+await addtoCart.click()
+//Validate alert is displaying
+await expect(alert).toBeVisible();
+//Click on cart icon
+await CartIcon.click()
+
+
+})
