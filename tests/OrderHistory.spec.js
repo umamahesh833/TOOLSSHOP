ea113fbb3db70cec1ad89218ee58713991e2ee2d
uma mahesh Kadiyala <umamahesh833@gmail.com>
2026-05-05 20:17:31 +0100

    Adding a new test to OrderHistory - Lakshman

--- tests/OrderHistory.spec.js
+++ tests/OrderHistory.spec.js
@@ -20,3 +20,24 @@ test('Validating My Orders or “Order History”', async ({ page }) => {
 
   //mahesh
 });
+
+
+test('Lakshman Test', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/');
+  await page.locator('[data-test="nav-sign-in"]').click();
+  await page.locator('[data-test="email"]').click();
+  await page.locator('[data-test="email"]').fill('umamahesh833@gmail.com');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('SoftwareTesting@123456789');
+  await page.locator('[data-test="login-form"]').getByRole('button').filter({ hasText: /^$/ }).click();
+  await page.locator('[data-test="login-submit"]').click();
+  await page.locator('[data-test="nav-invoices"]').click();
+
+ //await page.getByRole('link', { name: 'Details' }).click();
+
+  const InvoiceNumber =  page.locator('//table/tbody/tr/td[1]')
+
+  await expect(InvoiceNumber).toHaveText('INV-2026000012');
+
+  //mahesh
+});
