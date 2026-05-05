93b743ea693504fe28e3ed07f60d8d1a07cced70
uma mahesh Kadiyala <umamahesh833@gmail.com>
2026-05-05 20:49:21 +0100

    Removed all the wait times in all tests

--- tests/FirstUItest.spec.js
+++ tests/FirstUItest.spec.js
@@ -4,7 +4,6 @@ import{test, expect} from '@playwright/test'
 test('My first mahesh Test ',async ({page})=>{
 //Launch the browser and navigate to our application
 await page.goto("https://practicesoftwaretesting.com/")
-await page.waitForTimeout(5000)
 await expect(page).toHaveTitle(/Testing/);
 
 await page.getByText("Sign in").click()
@@ -15,20 +14,20 @@ await page.getByText("Sign in").click()
 // await page.waitForTimeout(2000)
 
 // await page.getByRole("button", {name:"Login"}).click()
-await page.waitForTimeout(2000)
+
 
 await page.getByRole("link", {name:"Register your account"}).click()
 
 //Entering firname
 await page.locator("input#first_name").fill("Automation")
 await page.locator("input[data-test='last-name']").fill("Automation")
-await page.waitForTimeout(2000)
+
 
 
 //Selecting an item from a drop down
 const CoutryDropDown = page.locator('#country')
 await CoutryDropDown.selectOption("Denmark")//selecting item using label
-await page.waitForTimeout(2000)
+
 // await CoutryDropDown.selectOption("FR")//selecting item using value
 // await page.waitForTimeout(2000)
 // await CoutryDropDown.selectOption({index:5})//selecting item using index
@@ -46,10 +45,8 @@ console.log("error message - "+await DoBerror.textContent())
 test('Radio Button Test ',async ({page})=>{
 //Launch the browser and navigate to our application
 await page.goto("https://automationexercise.com/signup")
-await page.waitForTimeout(5000)
 await page.getByRole("button", {name:"Consent"}).click()
 await page.getByPlaceholder("Name").fill("Random9987@gmail.com")
-await page.waitForTimeout(1000)
 await page.locator("[data-qa='signup-email']").fill("Random9987@gmail.com")
 
 const SignUp = page.getByRole("button", {name:'Signup'})
@@ -69,7 +66,7 @@ console.log("the status of radio button Mrs is - "+await mrsRadio.isChecked())
 //Select a check box
 const NewsLetterCheckBox = page.locator('#newsletter')
 await NewsLetterCheckBox.click()
-await page.waitForTimeout(3000)
+
 await NewsLetterCheckBox.uncheck()
 })
 
@@ -78,10 +75,10 @@ await NewsLetterCheckBox.uncheck()
 test('submit Button Test ',async ({page})=>{
 //Launch the browser and navigate to our application
 await page.goto("https://automationexercise.com/signup")
-await page.waitForTimeout(5000)
+
 await page.getByRole("button", {name:"Consent"}).click()
 await page.getByPlaceholder("Name").fill("Random9987@gmail.com")
-await page.waitForTimeout(1000)
+
 await page.locator("[data-qa='signup-email']").fill("Random9987@gmail.com")
 
 const SignUp = page.getByRole("button", {name:'Signup'})
@@ -101,6 +98,5 @@ console.log("the status of radio button Mrs is - "+await mrsRadio.isChecked())
 //Select a check box
 const NewsLetterCheckBox = page.locator('#newsletter')
 await NewsLetterCheckBox.click()
-await page.waitForTimeout(3000)
 await NewsLetterCheckBox.uncheck()
 })
