4837376cb0d8f69f16574ea974ececda8c51c3aa
Baby Rupa Sri <babyrupasri.kadiyala@gmail.com>
2026-05-28 14:16:46 +0100

    deleted

--- tests/productdetailspage.spec.js
+++
@@ -1,8 +0,0 @@
-//Validating the page displaying product name, image, price and description
-// 1.Launch the application.
-// 2.Go to home page
-// 3.Validate whether we can see product images or not
-// 4.Validate whether we can see product discription or not
-// 5.Validate whether the product include price or not
-// 6. Validate whether we can see product name or not"
-
